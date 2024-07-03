876edfa523c84be00edd9561b3f31b5e18d62dc5
Johannes Krottmayer <johannes@krotti42.com>
2024-07-03 04:57:48 +0200

    gpio: add main
    Add initial main.
    
    FILES:
    A  gpio/main.c
    
    Signed-off-by: Johannes Krottmayer <johannes@krotti42.com>

---
+++ gpio/main.c
@@ -0,0 +1,48 @@
+/**
+ *
+ * Copyright (c) 2024 Johannes Krottmayer <johannes@krotti42.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+#include <board.h>
+#include <gpio.h>
+
+void main(void)
+{
+    int pin_state = 0;
+    
+    board_setup();
+    gpio_init(GPIOB);
+    gpio_set_mode(GPIOB, 14, GPIO_MODE_OUTPUT);
+    gpio_set_pin(GPIOB, 14, 0);
+    gpio_init(GPIOC);
+    gpio_set_mode(GPIOC, 9, GPIO_MODE_ALT);
+    gpio_set_alternate(GPIOC, 9, 0);
+    gpio_set_mode(GPIOC, 13, GPIO_MODE_INPUT);
+    gpio_init(GPIOE);
+    gpio_set_mode(GPIOE, 1, GPIO_MODE_OUTPUT);
+    
+    while (1) {
+        if (gpio_get_pin(GPIOC, 13)) {
+            if (pin_state) {
+                pin_state = 0;
+                gpio_set_pin(GPIOE, 1, 0);
+            } else {
+                gpio_set_pin(GPIOE, 1, 1);
+                pin_state = 1;
+            }
+        }
+    }
+}
