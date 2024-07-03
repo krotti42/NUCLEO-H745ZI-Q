ee04fa68be534bfbdd77150184954d4e25f2796b
Johannes Krottmayer <johannes@krotti42.com>
2024-07-03 05:31:48 +0200

    gpio: add control Makefile
    Add initial control Makefile.
    
    FILES:
    A  gpio/files.mk
    
    Signed-off-by: Johannes Krottmayer <johannes@krotti42.com>

---
+++ gpio/files.mk
@@ -0,0 +1,10 @@
+#
+# STMicoelectronics NUCLEO-H745I-Q bare-metal examples (GPIO)
+# 
+# C files
+#
+
+CSRC_TARGET_CM7			+= ./board.c
+CSRC_TARGET_CM7			+= ./gpio.c
+CSRC_TARGET_CM7			+= ./heartbeat.c
+CSRC_TARGET_CM7			+= ./main.c
