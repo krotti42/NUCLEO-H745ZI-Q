4ca344d98ef643ec33e9a395125a983ff13baed7
Johannes Krottmayer <johannes@krotti42.com>
2024-07-03 08:54:12 +0200

    gpio/main.c: cleanup main
    Cleanup main.
    
    FILES:
    M  gpio/main.c
    
    Signed-off-by: Johannes Krottmayer <johannes@krotti42.com>

--- gpio/main.c
+++ gpio/main.c
@@ -19,28 +19,57 @@
 #include <board.h>
 #include <gpio.h>
 
+/* Blue button */
+#define USER_BUTTON_PORT        GPIOC
+#define USER_BUTTOM_PIN         13
+
+/* Yellow LED */
+#define USER_LED_PORT           GPIOE
+#define USER_LED_PIN            1
+
+/* Heartbeat (red LED) */
+#define HEARTBEAT_PORT          GPIOB
+#define HEARTBEAT_PIN           14
+
+/* MCO2 output */
+#define MCO2_PORT               GPIOC
+#define MCO2_PIN                9
+
 void main(void)
 {
     int pin_state = 0;
     
     board_setup();
-    gpio_init(GPIOB);
-    gpio_set_mode(GPIOB, 14, GPIO_MODE_OUTPUT);
-    gpio_set_pin(GPIOB, 14, 0);
-    gpio_init(GPIOC);
-    gpio_set_mode(GPIOC, 9, GPIO_MODE_ALT);
-    gpio_set_alternate(GPIOC, 9, 0);
-    gpio_set_mode(GPIOC, 13, GPIO_MODE_INPUT);
-    gpio_init(GPIOE);
-    gpio_set_mode(GPIOE, 1, GPIO_MODE_OUTPUT);
+    
+    /* Setup heartbeat pin */
+    gpio_init(HEARTBEAT_PORT);
+    gpio_set_mode(HEARTBEAT_PORT, HEARTBEAT_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_pin(HEARTBEAT_PORT, HEARTBEAT_PIN, 0);
+    
+    /* Setup MCO2 output pin */
+    gpio_init(MCO2_PORT);
+    gpio_set_mode(MCO2_PORT, MCO2_PIN, GPIO_MODE_ALT);
+    gpio_set_alternate(MCO2_PORT, MCO2_PIN, 0);
+    
+    /* Setup blue button */
+    gpio_init(USER_BUTTON_PORT);
+    gpio_set_mode(USER_BUTTON_PORT, USER_BUTTOM_PIN, GPIO_MODE_INPUT);
+    
+    /* Setup yellow LED */
+    gpio_init(USER_LED_PORT);
+    gpio_set_mode(USER_LED_PORT, USER_LED_PIN, GPIO_MODE_OUTPUT);
     
     while (1) {
-        if (gpio_get_pin(GPIOC, 13)) {
+        if (gpio_get_pin(USER_BUTTON_PORT, USER_BUTTOM_PIN)) {
+            /**
+             * TODO: Add delay function for button bouncing
+             */
+            
             if (pin_state) {
                 pin_state = 0;
-                gpio_set_pin(GPIOE, 1, 0);
+                gpio_set_pin(USER_LED_PORT, USER_LED_PIN, 0);
             } else {
-                gpio_set_pin(GPIOE, 1, 1);
+                gpio_set_pin(USER_LED_PORT, USER_LED_PIN, 1);
                 pin_state = 1;
             }
         }
